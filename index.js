293ce8a21bacb127e9440df3be8fc696be2cea9e
timlin <timlin1972@gmail.com>
2020-11-29 23:33:57 +0800

    To use graphql-mgr

--- index.js
+++ index.js
@@ -2,28 +2,16 @@ const { ApolloServer, gql } = require('apollo-server-express');
 
 const MODULE_NAME = 'graphql-server';
 
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
- 
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
-};
-
 const DEF_LOGGER = null;
 const DEF_APP = null;
 const DEF_PORT = 3010;
+const DEF_GRAPHQL_MGR = null;
 
 const DEF_CONFIGS = {
-    logger: DEF_LOGGER,
-    app: DEF_APP,
-    port: DEF_PORT,
+  logger: DEF_LOGGER,
+  app: DEF_APP,
+  port: DEF_PORT,
+  graphqlMgr: DEF_GRAPHQL_MGR,
 }
 
 class GraphqlServer {
@@ -31,18 +19,27 @@ class GraphqlServer {
     this.logger = configs.logger || DEF_LOGGER;
     this.app = configs.app || DEF_APP;
     this.port = configs.port || DEF_PORT;
+    this.graphqlMgr = configs.graphqlMgr || DEF_GRAPHQL_MGR;
 
-    this.graphqlServer = new ApolloServer({ typeDefs, resolvers });
+    this.log('info', 'Initialized');
   }
 
   start() {
     return new Promise((resolve, reject) => {
-        const { app, port } = this;
+        const { app, graphqlMgr, port } = this;
         if (!app) {
             this.log('error', `Express web server must be provided.`);
             reject(new Error('Express web server must be provided.'))
             return;
         }
+
+        if (!graphqlMgr) {
+          this.log('error', `Graphql mgr must be provided.`);
+          reject(new Error('Graphql mgr must be provided.'))
+          return;
+        }
+
+        this.graphqlServer = new ApolloServer(graphqlMgr.getSchema());
 
         this.graphqlServer.applyMiddleware({ app });
         
